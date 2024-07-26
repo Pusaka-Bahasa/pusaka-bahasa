7f925bbff4f0909d9320de39f69d625973da8c68
ckckckcz <rriovld@gmail.com>
2024-07-26 10:13:55 +0700

    update: navbar background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import "../styles/index.css";
+import "../styles/output.css";
 import Logo from "../images/Logo.png";
 
 export default function Navbar() {
@@ -10,52 +10,25 @@ export default function Navbar() {
           <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Pusaka Bahasa</span>
         </a>
         <div className="md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse lg:block sm:hidden hidden">
-          <span className="flex flex-col-1 lg:gap-2 font-medium sm:gap-1 md:gap-1 gap- items-center lg:px-2.5 lg:py-2.5 sm:px-2.5 sm:py-2.5 px-2 py-1 md:px-2.5 md:py-2.5" style={{ background: "#F5F7F8", borderRadius: "10px" }}>
+          <span className="flex flex-col-1 lg:gap-2 font-medium sm:gap-1 md:gap-1 gap- items-center lg:px-4 lg:py-2 sm:px-2.5 sm:py-2.5 px-2 py-1 md:px-2.5 md:py-2.5" style={{ background: "#F5F7F8", borderRadius: "10px" }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="w-4 h-4">
               <path fill="#357977" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
             </svg>
-            <a
-              href="#"
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              aria-current="page"
-            >
+            <a href="#" className="block py-2 px-3 text-gray-900 rounded md:p-0  dark:text-white dark:border-gray-700" aria-current="page">
               Indonesia
             </a>
           </span>
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col mt-4 font-medium border border-gray-100 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                aria-current="page"
-              >
-                Home
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 navbg">
+                Beranda
               </a>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Contact
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 navbg">
+                Tentang Kami
               </a>
             </li>
           </ul>
