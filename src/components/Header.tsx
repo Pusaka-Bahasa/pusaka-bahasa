61d187ec29d8a52faab13f607fee2fafdd8a0120
ckckckcz <rriovld@gmail.com>
2024-07-26 21:04:16 +0700

    add: two button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,12 +16,20 @@ export default function Header() {
           <h1 className="mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl lg:text-6xl dark:text-white aight1">
             Jelajahi <a className="paraf">Keindahan</a> Bahasa Daerah
           </h1>
-          <p className="mb-4 lg:text-md sm:text-sm font-medium text-gray-300">
+          <p className="mb-8 lg:text-md sm:text-sm font-medium text-gray-300">
             Pusaka Bahasa fokus pada kekayaan linguistik dan budaya Indonesia, PusakaBahasa menyediakan berbagai kursus dan materi untuk mempelajari bahasa-bahasa daerah, sambil menawarkan fitur-fitur yang mendukung keterlibatan aktif dan
             pengalaman belajar yang mendalam.
           </p>
+          <div className="buttonSection flex flex-col-2 mb-4">
+            <button type="button" className="text-black bg-hommy font-medium rounded-full lg:text-1xl sm:text-sm md:text-md px-6 py-2.5 text-center me-2 mb-2">
+              Ayo Mulai !
+            </button>
+            <button type="button" className="py-2.5 px-6 me-2 mb-2 lg:text-1xl sm:text-sm md:text-md font-medium text-white rounded-full bg-sechomy">
+              Lihat Sejarah
+            </button>
+          </div>
         </div>
-        <div className="container mx-auto bento">
+        <div className="container mx-auto bento sm:hidden lg:block hidden">
           <div className="bento-grid">
             <div className="bento-grid-item row-span-2 col-span-2">
               <img src={Daerah1} alt="Daerah 1" />
