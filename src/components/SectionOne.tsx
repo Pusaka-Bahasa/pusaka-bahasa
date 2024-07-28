0ef6b298011db4dc943b0fa2763cfa4572821fa2
ckckckcz <rriovld@gmail.com>
2024-07-28 19:13:27 +0700

    update: text title

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -5,11 +5,9 @@ export default function SectionOne() {
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Siap untuk <a className="paraf2">petualangan</a> bahasa ?
+          Mari menjelajahi <a className="paraf2">keindahan</a> bahasa daerah
         </h1>
-        <p className="mb-8 mt-8 lg:text-md sm:text-sm font-medium text-gray-500 sm:px-16 lg:px-48 ">
-          Setiap langkah membawa kamu lebih dekat pada pemahaman mendalam tentang warisan budaya kita. Pilih bahasa yang ingin kamu pelajari dan mulailah petualangan bahasa kamu sekarang !
-        </p>
+        <p className="mb-8 mt-8 lg:text-md sm:text-sm font-medium text-gray-500 sm:px-16 lg:px-48 ">Setiap langkah membawa kamu lebih dekat pada pemahaman mendalam tentang warisan bahasa daerah di Indonesia</p>
         <div className="wrapper-left">
           <div className="itemLeft item1">
             <div className="bg-prim p-6 flex items-center justify-center text-center">
