6cf8df3b5fecc3ec3891e590455dd39ec9f1b0d1
ckckckcz <rriovld@gmail.com>
2024-09-08 15:08:54 +0700

    feat: configuration and new navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
