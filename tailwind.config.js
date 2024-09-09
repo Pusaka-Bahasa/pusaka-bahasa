f3ae07a3deff78cb326a20b8f0a55c4684220f84
ckckckcz <rriovld@gmail.com>
2024-09-09 17:35:10 +0700

    feat: header style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./public/index.html",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ["DM Serif Display", "serif"],
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
