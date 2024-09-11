e889f2892cd98b92373d9f4bdb348264eb698880
ckckckcz <rriovld@gmail.com>
2024-09-11 18:27:45 +0700

    feat: mapping data courses done

---
+++ src/data/topCourses.js
@@ -0,0 +1,34 @@
+export const courses = [
+  {
+    title: "Mengenal Bahasa Jawa: Dari Dasar hingga Lancar",
+    thumbnail: "/courseThumbnail/MengenalBahasaJawaDariDasarhinggaLancar.png",
+    mentorImage: "/mentorPicture/mentor1.png",
+    mentorName: "Rania Yamin",
+    price: "Rp. 50.000",
+    rating: 5,
+  },
+  {
+    title: "Bahasa Madura: Belajar Berbicara hingga Lancar",
+    thumbnail: "/courseThumbnail/BahasaMaduraBelajarBerbicarahinggaLancar.png",
+    mentorImage: "/mentorPicture/mentor1.png",
+    mentorName: "Bima Saputra",
+    price: "Rp. 50.000",
+    rating: 5,
+  },
+  {
+    title: "Bahasa Sasak Lombok: Pemahaman Mudah",
+    thumbnail: "/courseThumbnail/BahasaSasakLombokPemahamanMudah.png",
+    mentorImage: "/mentorPicture/mentor1.png",
+    mentorName: "Dian Kartika",
+    price: "Rp. 50.000",
+    rating: 5,
+  },
+  {
+    title: "Bahasa Bali: Pemahaman Dasar dan Pengucapan",
+    thumbnail: "/courseThumbnail/BahasaBaliPemahamanDasardanPengucapan.png",
+    mentorImage: "/mentorPicture/mentor1.png",
+    mentorName: "Sinta Maharani",
+    price: "Rp. 50.000",
+    rating: 5,
+  },
+];
